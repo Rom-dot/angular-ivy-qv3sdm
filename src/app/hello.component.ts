c7ced7f2fbad3fad5282c8c60b673bcf08b9989a
Romanus-dot <romange3100@gmail.com>
2020-08-20 15:42:46 -0400

    applications des maps au pipe

--- src/app/hello.component.ts
+++ src/app/hello.component.ts
@@ -1,50 +1,50 @@
-import { Component, Input } from '@angular/core';
-import { filter, map,  } from 'rxjs/operators';
-import { of, pipe } from 'rxjs';
+import { Component, Input } from "@angular/core";
+import { filter, map } from "rxjs/operators";
+import { of, pipe } from "rxjs";
 
 @Component({
-  selector: 'hello',
-  template: `<h1>Hello {{name}}!</h1>
-  <button (click)="pipeFonction()">Pipe Fonction</button>
-  <button (click)="mapFonction()">Map Fonction</button>
-  Regarder la console pour le resultat
-  
+  selector: "hello",
+  template: `
+    <h1>Hello {{ name }}!</h1>
+    <button (click)="pipeFonction()">Pipe Fonction</button>
+    <button (click)="mapFonction()">Map Fonction</button>
+    Regarder la console pour le resultat
   `,
-  styles: [`h1 { font-family: Lato; }`]
+  styles: [
+    `
+      h1 {
+        font-family: Lato;
+      }
+    `
+  ]
 })
-export class HelloComponent  {
+export class HelloComponent {
   @Input() name: string;
 
-  constructor(){}
+  constructor() {}
 
   pipeFonction() {
     const nums = of(1, 2, 3, 4, 5);
 
-// Create a function that accepts an Observable.
-const squareOddVals = pipe(
-  filter((n: number) => n % 2 !== 0),
-  map(n => n * n)
-);
+    // Create a function that accepts an Observable.
+    const squareOddVals = pipe(
+      //filter((n: number) => n % 2 !== 0),
+      map((n: number) => n + n),
+      map(n => n * n),
+      map(n => n+1)
+    );
 
-// Create an Observable that will run the filter and map functions
-const squareOdd = squareOddVals(nums);
+    // Create an Observable that will run the filter and map functions
+    const squareOdd = squareOddVals(nums);
 
-// Subscribe to run the combined functions
-squareOdd.subscribe(x => console.log(x));
-
+    // Subscribe to run the combined functions
+    squareOdd.subscribe(x => console.log(x));
   }
 
-mapFonction(){
-  const nums = of(1, 2, 3);
-
-const squareValues = map((val: number) => val * val);
-const squaredNums = squareValues(nums);
-
-squaredNums.subscribe(x => console.log(x));
+  mapFonction() {
+    const nums = of(1, 2, 3);
+    const squareValues = map((val: number) => val * val);
+    const squaredNums = squareValues(nums);
+    squaredNums.subscribe(x => console.log(x));
+  }
 }
-
-  
-  
-}
-
-
